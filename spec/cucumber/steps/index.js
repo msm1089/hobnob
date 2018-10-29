5723bce81e68bed10db000c960789245f94b2a08
Mark McPherson <msm1089@yahoo.co.uk>
2018-10-29 03:37:35 +0000

    WIP Implement Create User with Empty Payload

--- spec/cucumber/steps/index.js
+++ spec/cucumber/steps/index.js
@@ -1,25 +1,38 @@
+import superagent from 'superagent';
 import { When, Then } from 'cucumber';
+let request;
+let result;
+let error;
 
-When('the client creates a POST request to /users', function (callback) {
-  callback(null, 'pending');
+When('the client creates a POST request to /users', function() {
+  request = superagent('POST', 'localhost:8080/users');
 });
 
-When('attaches a generic empty payload', function (callback) {
-  callback(null, 'pending');
+When('attaches a generic empty payload', function() {
+  return undefined;
 });
 
-When('sends the request', function (callback) {
-  callback(null, 'pending');
+When('sends the request', function(callback) {
+  request
+    .then(response => {
+      result = response.res;
+      callback();
+    })
+    .catch(errResponse => {
+      error = errResponse.response;
+      callback();
+    });
 });
 
-Then('our API should respond with a 400 HTTP status code', function (callback) {
-  callback(null, 'pending');
+Then('our API should respond with a 400 HTTP status code', function() {
+  if (error.statusCode !== 400) {
+    throw new Error();
+  }
 });
 
-Then('the payload of the response should be a JSON object', function (callback) {
-  callback(null, 'pending');
-});
-
-Then('contains a message property which says "Payload should not be empty"', function (callback) {
-  callback(null, 'pending');
-});
+Then(
+  'contains a message property which says "Payload should not be empty"',
+  function(callback) {
+    callback(null, 'pending');
+  }
+);
