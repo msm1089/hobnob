ad91db01a8670d48bce89d26a7191e77125d8927
Mark McPherson <msm1089@yahoo.co.uk>
2018-11-10 13:53:51 +0000

    Check Create User endpoint for missing fields

--- spec/cucumber/steps/index.js
+++ spec/cucumber/steps/index.js
@@ -76,3 +76,21 @@ Then(/^contains a message property which says (?:"|')(.*)(?:"|')$/, function(
 When(/^without a (?:"|')([\w-]+)(?:"|') header set$/, function(headerName) {
   this.request.unset(headerName);
 });
+
+When(
+  /^attaches an? (.+) payload which is missing the ([a-zA-Z0-9, ]+) fields?$/,
+  function(payloadType, missingFields) {
+    const payload = {
+      email: 'e@ma.il',
+      password: 'password'
+    };
+    const fieldsToDelete = missingFields
+      .split(',')
+      .map(s => s.trim())
+      .filter(s => s !== '');
+    fieldsToDelete.forEach(field => delete payload[field]);
+    this.request
+      .send(JSON.stringify(payload))
+      .set('Content-Type', 'application/json');
+  }
+);
