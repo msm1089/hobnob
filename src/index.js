8a6d87b194c2aa207e315fbe0409d450826f39f3
Mark McPherson <msm1089@yahoo.co.uk>
2018-11-13 03:57:08 +0000

    Use test index for E2E tests

--- src/index.js
+++ src/index.js
@@ -86,7 +86,7 @@ app.post('/users', (req, res) => {
   }
   client
     .index({
-      index: 'hobnob',
+      index: process.env.ELASTICSEARCH_INDEX,
       type: 'user',
       body: req.body
     })
