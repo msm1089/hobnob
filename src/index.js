c25ffe4226e2551d42cdb64831aa28273bf921f3
Mark McPherson <msm1089@yahoo.co.uk>
2018-11-19 08:52:48 +0000

    Extract middlewares into modules#2

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import '@babel/polyfill';
 import express from 'express';
 import bodyParser from 'body-parser';
 import elasticsearch from 'elasticsearch';
+import checkEmptyPayload from './middlewares/check-empty-payload';
+import checkContentTypeIsSet from './middlewares/check-content-type-is-set';
+import checkContentTypeIsJson from './middlewares/check-content-type-is-json';
+import errorHandler from './middlewares/error-handler';
 
 const app = express();
 
@@ -11,47 +15,11 @@ const client = new elasticsearch.Client({
   }`
 });
 
-function checkEmptyPayload(req, res, next) {
-  if (['POST', 'PATCH', 'PUT'].includes(req.method) && req.headers['content-length'] === '0') {
-    res.status(400);
-    res.set('Content-Type', 'application/json');
-    res.json({
-      message: 'Payload should not be empty'
-    });
-  }
-  next();
-}
-
-function checkContentTypeIsSet(req, res, next) {
-  if (
-    req.headers['content-length'] &&
-    req.headers['content-length'] !== '0' &&
-    !req.headers['content-type']
-  ) {
-    res.status(400);
-    res.set('Content-Type', 'application/json');
-    res.json({
-      message: 'The "Content-Type" header must be set for requests with a non-empty payload'
-    });
-  }
-  next();
-}
-
-function checkContentTypeIsJson(req, res, next) {
-  if (!req.headers['content-type'].includes('application/json')) {
-    res.status(415);
-    res.set('Content-Type', 'application/json');
-    res.json({
-      message: 'The "Content-Type" header must always be "application/json"'
-    });
-  }
-  next();
-}
-
 app.use(checkEmptyPayload);
 app.use(checkContentTypeIsSet);
 app.use(checkContentTypeIsJson);
 app.use(bodyParser.json({ limit: 1e6 }));
+app.use(errorHandler);
 
 app.listen(process.env.SERVER_PORT, () => {
   // eslint-disable-next-line no-console
@@ -100,19 +68,4 @@ app.post('/users', (req, res) => {
       res.set('Content-Type', 'application/json');
       res.json({ message: 'Internal Server Error' });
     });
-});
-
-app.use((err, req, res, next) => {
-  if (
-    err instanceof SyntaxError &&
-    err.status === 400 &&
-    'body' in err &&
-    err.type === 'entity.parse.failed'
-  ) {
-    res.status(400);
-    res.set('Content-Type', 'application/json');
-    res.json({ message: 'Payload should be in JSON format' });
-    return;
-  }
-  next();
 });
---
+++ src/middlewares/check-content-type-is-set.js
@@ -0,0 +1,16 @@
+function checkContentTypeIsSet(req, res, next) {
+  if (
+    req.headers['content-length'] &&
+    req.headers['content-length'] !== '0' &&
+    !req.headers['content-type']
+  ) {
+    res.status(400);
+    res.set('Content-Type', 'application/json');
+    res.json({
+      message: 'The "Content-Type" header must be set for requests with a non-empty payload'
+    });
+  }
+  next();
+}
+
+export default checkContentTypeIsSet;
