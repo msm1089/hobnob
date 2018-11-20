116790104fc76d947acb9bcdef957ce1c304e5c0
Mark McPherson <msm1089@yahoo.co.uk>
2018-11-20 04:02:48 +0000

    Ensure Single-Responsibility Principle for handler

--- src/handlers/users/create.js
+++ src/handlers/users/create.js
@@ -1,29 +1,28 @@
 import ValidationError from '../../validators/errors/validation-error';
-import validate from '../../validators/users/create';
+import create from '../../engines/users/create';
 
 function createUser(req, res, db) {
-  const validationResults = validate(req);
-  if (validationResults instanceof ValidationError) {
-    res.status(400);
-    res.set('Content-Type', 'application/json');
-    return res.json({ message: validationResults.message });
-  }
-  db.index({
-    index: process.env.ELASTICSEARCH_INDEX,
-    type: 'user',
-    body: req.body
-  })
-    .then(result => {
-      res.status(201);
-      res.set('Content-Type', 'text/plain');
-      return res.send(result._id);
-    })
+  create(req, db)
+    .then(
+      result => {
+        res.status(201);
+        res.set('Content-Type', 'text/plain');
+        return res.send(result._id);
+      },
+      err => {
+        if (err instanceof ValidationError) {
+          res.status(400);
+          res.set('Content-Type', 'application/json');
+          return res.json({ message: err.message });
+        }
+        return undefined;
+      }
+    )
     .catch(() => {
       res.status(500);
       res.set('Content-Type', 'application/json');
       return res.json({ message: 'Internal Server Error' });
     });
-  return undefined;
 }
 
 export default createUser;
