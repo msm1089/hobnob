d7f3536bccac150d93d394a2ce8a3a0dc592ca5d
Mark McPherson <msm1089@yahoo.co.uk>
2019-04-25 15:09:18 +0100

    Implemented update profile unit tests

--- src/engines/profile/update/index.unit.test.js
+++ src/engines/profile/update/index.unit.test.js
@@ -2,11 +2,11 @@ import assert from 'assert';
 import ValidationError from '../../../validators/errors/validation-error';
 import generatevalidator, { VALIDATION_ERROR } from '../../../tests/stubs/validate';
 import generateESClientUpdateStub from '../../../tests/stubs/elasticsearch/client/update';
-import replace from '.';
+import update from '.';
 
 const USER_ID = 'USER_ID';
 
-describe('Engine - Profile - Replace', function() {
+describe('Engine - Profile - Update', function() {
   let req;
   let db;
   let validator;
@@ -23,7 +23,7 @@ describe('Engine - Profile - Replace', function() {
   describe('When invoked', function() {
     beforeEach(function() {
       validator = generatevalidator().valid;
-      return replace(req, db, validator, ValidationError);
+      return update(req, db, validator, ValidationError);
     });
     describe('should call the validator', function() {
       it('once', function() {
@@ -37,7 +37,7 @@ describe('Engine - Profile - Replace', function() {
   describe('When the validate function returns false', function() {
     beforeEach(function() {
       validator = generatevalidator().invalid;
-      promise = replace(req, db, validator, ValidationError);
+      promise = update(req, db, validator, ValidationError);
     });
     it('should return with a promise that rejects with a ValidationError', function() {
       return promise.catch(error => assert(error, VALIDATION_ERROR));
@@ -49,7 +49,7 @@ describe('Engine - Profile - Replace', function() {
     });
     describe('Continues execution', function() {
       beforeEach(function() {
-        promise = replace(req, db, validator, ValidationError);
+        promise = update(req, db, validator, ValidationError);
         return promise;
       });
       describe('should call client.update()', function() {
@@ -58,12 +58,8 @@ describe('Engine - Profile - Replace', function() {
           type: 'user',
           id: USER_ID,
           body: {
-            script: {
-              lang: 'painless',
-              params: {
-                profile: {}
-              },
-              source: 'ctx._source.profile = params.profile'
+            doc: {
+              profile: {}
             }
           }
         };
@@ -78,7 +74,7 @@ describe('Engine - Profile - Replace', function() {
     describe('When the client.update() operation is successful', function() {
       beforeEach(function() {
         db = { update: generateESClientUpdateStub.success() };
-        promise = replace(req, db, validator, ValidationError);
+        promise = update(req, db, validator, ValidationError);
         return promise;
       });
       it('should return with a promise that resolves to undefined', function() {
@@ -88,7 +84,7 @@ describe('Engine - Profile - Replace', function() {
     describe('When the user does not exists', function() {
       beforeEach(function() {
         db = { update: generateESClientUpdateStub.notFound() };
-        promise = replace(req, db, validator, ValidationError);
+        promise = update(req, db, validator, ValidationError);
       });
       describe('should return with a promise that rejects', function() {
         it('with an Error object', function() {
@@ -102,7 +98,7 @@ describe('Engine - Profile - Replace', function() {
     describe('When the client.update operation is otherwise unsuccessful', function() {
       beforeEach(function() {
         db = { update: generateESClientUpdateStub.failure() };
-        promise = replace(req, db, validator, ValidationError);
+        promise = update(req, db, validator, ValidationError);
       });
       describe('should return with a promise that rejects', function() {
         it('with an Error object', function() {
