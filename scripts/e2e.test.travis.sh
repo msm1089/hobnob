93454245a02de5bfc7b7743f97ed59ad303a8503
Mark McPherson <msm1089@yahoo.co.uk>
2019-04-28 23:37:37 +0100

    Added e2e test script for Travis

--- scripts/e2e.test.travis.sh
+++ scripts/e2e.test.travis.sh
@@ -1,7 +1,2 @@
 #!/usr/bin/env bash
-yarn run serve &
-until ss -lnt | grep -q :$SERVER_PORT; do
-  sleep $RETRY_INTERVAL
-done
-npx cucumber-js spec/cucumber/features --require-module @babel/register --require spec/cucumber/steps
-kill -15 0
+yarn run serve & npx cucumber-js spec/cucumber/features --require-module @babel/register --require spec/cucumber/steps
