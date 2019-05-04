5e48904a02b4cd022475763858a558e7c14d89c0
Mark McPherson <msm1089@yahoo.co.uk>
2019-05-04 12:16:36 +0100

    Most scripts now working, still problems with e2e coverage

--- scripts/e2e.prepare.sh
+++ scripts/e2e.prepare.sh
@@ -1,20 +1,37 @@
 #!/usr/bin/env bash
 
- # Make sure the port is not already bound
-if ss -lnt | grep -q :$SERVER_PORT; then
-  echo "Another process is already listening to port $SERVER_PORT"
-  exit 1;
+# Make sure the port is not already bound
+if [[ "$OSTYPE" == "msys" ]]; then
+        if netstat -aon | grep "0.0.0.0:$SERVER_PORT" | grep "LISTENING"; then
+                pm2 delete all
+        fi
+else
+        if ss -lnt | grep -q :$SERVER_PORT; then
+                if ss -lnt | grep -q :$SERVER_PORT; then
+                        echo "Another process is already listening to port $SERVER_PORT"
+                        exit 1;
+                fi
+        fi
 fi
 
  RETRY_INTERVAL=${RETRY_INTERVAL:-0.2}
 
- if ! systemctl is-active --quiet elasticsearch; then
-  sudo systemctl start elasticsearch
-  # Wait until Elasticsearch is ready to respond
-  until curl --silent $ELASTICSEARCH_HOSTNAME:$ELASTICSEARCH_PORT -w "" -o /dev/null; do
-    sleep $RETRY_INTERVAL
-  done
+if [[ "$OSTYPE" == "msys" ]]; then
+        if ! curl $ELASTICSEARCH_HOSTNAME:$ELASTICSEARCH_PORT; then
+               echo es not running
+               sc start elasticsearch
+        fi
+else
+        if ! systemctl is-active --quiet elasticsearch; then
+                sudo systemctl start elasticsearch
+                
+        fi
 fi
 
+# Wait until Elasticsearch is ready to respond
+until curl $ELASTICSEARCH_HOSTNAME:$ELASTICSEARCH_PORT; do
+        sleep $RETRY_INTERVAL
+done
+
  # Clean the test index (if it exists)
-curl --silent -o /dev/null -X DELETE "$ELASTICSEARCH_HOSTNAME:$ELASTICSEARCH_PORT/$ELASTICSEARCH_INDEX"
+curl -X DELETE "$ELASTICSEARCH_HOSTNAME:$ELASTICSEARCH_PORT/$ELASTICSEARCH_INDEX"
