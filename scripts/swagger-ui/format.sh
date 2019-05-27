19bc45ff4334610baa748f4546b73c9ea5b9e11f
Mark McPherson <msm1089@yahoo.co.uk>
2019-05-27 03:07:52 +0100

    Tidied up style injection and hide Try It Now button

--- scripts/swagger-ui/format.sh
+++ scripts/swagger-ui/format.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 sed -i "s!https://petstore.swagger.io/v2/swagger.json!$SERVER_EXTERNAL_PROTOCOL://$SERVER_EXTERNAL_HOSTNAME:$SERVER_EXTERNAL_PORT/openapi.yaml!g" docs/dist/index.html
-sed -i '/<\/head>/i<style>.topbar { display: none; }<\/style>' docs/dist/index.html
+sed -i '/<\/style>/i\\n      .topbar { display: none; }\n      .try-out__btn { display: none }' docs/dist/index.html
